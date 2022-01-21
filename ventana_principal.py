f8e844d0b39ee73c9deb9290182104c8f921f22e
limbers may <limbermay21@gmail.com>
2022-01-21 10:49:16 -0600

    Se agregó la configuración básica de la ventana principal (titulo y dimensiones)

--- ventana_principal.py
+++ ventana_principal.py
@@ -3,5 +3,10 @@ from tkinter import *
 class Ventana(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+        
+        # Configuración básica
+        master.title("Organizador de archivos")
+        master.geometry("500x500")
 
         # Partes del programa
+        
