db3664b487db45e52604c3a970704a5133cc0ca1
limbers may <limbermay21@gmail.com>
2022-01-21 10:55:13 -0600

    se agregaron las clases titulo, tabla y boton en la ventana principal para los testeos

--- ventana_principal.py
+++ ventana_principal.py
@@ -1,5 +1,9 @@
 from tkinter import *
 
+from botones_inferiores import Boton
+from tabla import Tabla
+from titulo import Titulo
+
 class Ventana(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
@@ -9,4 +13,11 @@ class Ventana(Frame):
         master.geometry("500x500")
 
         # Partes del programa
+        titulo = Titulo(self)
+        tabla = Tabla(self)
+        botones = Boton(self)
+
+        titulo.grid(row=1, column=1)
+        tabla.grid(row=2, column=2)
+        botones.grid(row=3, column=3)
         
