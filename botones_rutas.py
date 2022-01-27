26f10889ac06cba31a90de20f482fd3f3fc24c31
limbers may <limbermay21@gmail.com>
2022-01-26 18:49:59 -0600

    se agregaron las configuraciones del frame BotonRuta

--- botones_rutas.py
+++ botones_rutas.py
@@ -4,3 +4,9 @@ class BotonRuta(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         
+        # Configuraciones
+        self.config(
+            bg="white",
+            width=500,
+            height=50
+        )
