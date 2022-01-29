0ea5d35301c1b2a1de2610291a983c55022cf5c7
limbers may <limbermay21@gmail.com>
2022-01-28 21:52:05 -0600

    Se agregó la configuración básica del frame Tabla (width, height y bg)

--- tabla.py
+++ tabla.py
@@ -4,3 +4,8 @@ class Tabla(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         
+        self.config(
+            width=480,
+            height=150,
+            bg="#e7e6e1"
+        )
