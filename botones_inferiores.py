f94231586dbdb1ca8c06fd0e98c6226f3a7e1dd1
limbers may <limbermay21@gmail.com>
2022-01-28 21:58:44 -0600

    se agregó la configuración básica del frame (width, height y bg)

--- botones_inferiores.py
+++ botones_inferiores.py
@@ -3,3 +3,9 @@ from tkinter import *
 class Boton(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        self.config(
+        width=500,
+        height=50,
+        bg="#e7e6e1"
+        )
