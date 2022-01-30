ff5236045310db8b9ebf0e311cb1a1dd47113868
limbers may <limbermay21@gmail.com>
2022-01-29 19:04:32 -0600

    se agregaron las configuraciones de estilo del boton iniciar y configuraciones

--- botones_inferiores.py
+++ botones_inferiores.py
@@ -15,3 +15,16 @@ class Boton(Frame):
         self.iniciar_button = Button(self, text="Iniciar")
         self.imagen = PhotoImage(file="recursos/icono.png")
         self.configuraciones_button = Button(self, text="", image=self.imagen)
+
+        # Configuracion de estilos de los objetos
+        self.iniciar_button.config(
+            fg="white",
+            borderwidth=0,
+            bg="cadetblue"
+        )
+
+        self.configuraciones_button.config(
+            activebackground="#2c2b33", 
+            bg="#2c2b33",
+            borderwidth=0
+        )
