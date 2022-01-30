facb1712f6f466821b6bd5969af7f1c86ac2e2c7
limbers may <limbermay21@gmail.com>
2022-01-29 18:56:18 -0600

    se agregaron los 2 botones inferiores y el texto del estado (estado, iniciar y configuraciones

--- botones_inferiores.py
+++ botones_inferiores.py
@@ -9,3 +9,9 @@ class Boton(Frame):
         height=50,
         bg="#e7e6e1"
         )
+
+        # Partes del programa
+        self.operacion_label = Label(self, text="copiar")
+        self.iniciar_button = Button(self, text="Iniciar")
+        self.imagen = PhotoImage(file="recursos/icono.png")
+        self.configuraciones_button = Button(self, text="", image=self.imagen)
