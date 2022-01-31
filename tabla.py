baf7eae0905e1c9ad01968f51356eac100683e11
limbers may <limbermay21@gmail.com>
2022-01-31 17:07:04 -0600

    creamos la vista de árbol (treeview) y editamos las columnas y cabeceras

--- tabla.py
+++ tabla.py
@@ -14,3 +14,22 @@ class Tabla(Frame):
         # Creamos la tabla
         self.tabla_scrollbar = Scrollbar(self)
         self.tabla_scrollbar.pack(side=RIGHT, fill=Y)
+
+        self.tabla = Treeview(self, columns=("Carpeta", "Similitud"), yscrollcommand=self.tabla_scrollbar.set, selectmode="none")
+
+        # Creamos las columnas
+        self.tabla.column("#0", width=150, anchor=CENTER)
+        self.tabla.column("Carpeta", width=150, anchor=CENTER)
+        self.tabla.column("Similitud", width=150, anchor=CENTER)
+
+        # Editamos los cabeceros
+        self.tabla.heading("#0", text="Archivo")
+        self.tabla.heading("Carpeta", text="Carpeta")
+        self.tabla.heading("Similitud", text="Similitud")
+
+        self.tabla.pack()
+
+        # Configuramos el scrollbar
+        self.tabla_scrollbar.config(
+        command=self.tabla.yview
+        )
