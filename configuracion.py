7c2287e57f859e8bb361fde13712e1a0aa635c03
limber <limbermay21@gmail.com>
2022-02-01 16:15:27 -0600

    se agregaron los métodos para obtener y modificar la ruta de origen del fichero json

--- configuracion.py
+++ configuracion.py
@@ -18,3 +18,11 @@ class Configuracion:
     # Método para eliminar un observador
     def eliminar_observador(self, observador):
         self.observadores.remove(observador)
+
+    # Método para obtener la ruta de origen
+    def obtener_ruta_origen(self):
+        return self.fichero.obtener_valor("directorios", "rutaOrigen")
+    
+    # Método para modificar la ruta de origen
+    def modificar_ruta_origen(self, ruta):
+        self.fichero.modificar_json("directorios", "rutaOrigen", ruta)
