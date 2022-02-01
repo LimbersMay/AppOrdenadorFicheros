d863719df885d264cc3604e18e79b4267a55b239
limber <limbermay21@gmail.com>
2022-02-01 16:05:47 -0600

    se creó la función modificar_json para interactuar directamente con los valores y modificarlos

--- fichero.py
+++ fichero.py
@@ -5,3 +5,15 @@ from io import open
 class Fichero:
     def __init__(self, ruta):
         self.ruta = ruta
+
+    def modificar_json(self, clave_1, clave_2, informacion):
+        # Abrimos el fichero y cargamos la información en formato json
+        with open(self.ruta, "r") as fichero:
+            self.cargar = json.loads(fichero.read())
+        
+        # Modificamos la información del Json
+        self.cargar[clave_1][clave_2] = informacion
+
+        # Guardamos la información nuevamente en el fichero Json
+        with open(self.ruta, "w") as fichero:
+            fichero.write(json.dumps(self.cargar, indent=4))
