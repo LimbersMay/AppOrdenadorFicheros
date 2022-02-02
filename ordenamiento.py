7657fc4714ccbe788630cbd9999bd77290cc38e7
limber <limbermay21@gmail.com>
2022-02-02 11:35:32 -0600

    se creó la plantilla de la clase y se agregaron los import necesarios

---
+++ ordenamiento.py
@@ -0,0 +1,8 @@
+# Clase que se encargará de ordenar todos los archivos que se encuentren en la ruta deseada a la ruta de destino
+
+from configuracion import Configuracion
+from manipulacionarchivo import ManipulacionArchivo
+
+class Ordenamiento:
+    def __init__(self):
+        pass
