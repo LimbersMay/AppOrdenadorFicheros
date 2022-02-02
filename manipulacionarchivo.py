291e003fe5d9862eac2256affb4f2e5aba04c6bf
limber <limbermay21@gmail.com>
2022-02-02 11:29:52 -0600

    se agregaron los métodos para enviar la ruta de origen, destino y el nombre de archivo

--- manipulacionarchivo.py
+++ manipulacionarchivo.py
@@ -16,3 +16,13 @@ class ManipulacionArchivo:
 
     def copiar_fichero(self):
         shutil.copy(self.ruta_origen + self.nombre_archivo, self.ruta_destino + self.nombre_archivo)
+    
+    # Métodos de rutas
+    def set_ruta_origen(self, ruta_origen: str):
+        self.ruta_origen = ruta_origen + "\\"
+
+    def set_ruta_destino(self, ruta_destino: str):
+        self.ruta_destino = ruta_destino + "\\"
+
+    def set_nombre_archivo(self, nombre_archivo: str):
+        self.nombre_archivo = nombre_archivo
