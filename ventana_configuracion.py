fb4d344fd4bfb6a9e23ece14562f9290149fe7b0
limber <limbermay21@gmail.com>
2022-02-05 14:08:37 -0600

    se agregó la configuración básica de la ventana secundaria

--- ventana_configuracion.py
+++ ventana_configuracion.py
@@ -1,5 +1,13 @@
 from tkinter import *
 
-class VentanaConfiguracion(Label):
+class VentanaConfiguracion(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        # Configuraciones del master
+        master.title("Configuraciones")
+        master.geometry("370x350")
+        master.config(bg="#2c2b33")
+
+        # Configuraciones del frame
+        self.config(bg="#2c2b33")
