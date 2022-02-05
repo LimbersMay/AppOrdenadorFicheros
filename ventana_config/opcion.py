79100ef831a329834547a1f2ae407d00b8d5cb93
limber <limbermay21@gmail.com>
2022-02-05 14:45:13 -0600

    se agregaron los atributos texto y lista_opciones a la clase opcion

--- ventana_config/opcion.py
+++ ventana_config/opcion.py
@@ -1,6 +1,16 @@
 from tkinter import *
 
 class Opcion(Frame):
-    def __init__(self, master):
+    def __init__(self, master, texto = "", lista_combobox = None):
         Frame.__init__(self, master)
 
+        # Atributos de la clase
+        self.texto = texto
+        self.lista_combobox = lista_combobox
+
+        # Configuraciones del frame
+        self.config(
+            bg="white",
+            width=350,
+            height=30
+        )
