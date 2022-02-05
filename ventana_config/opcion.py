b0eec2a7bb834f530e73c131a885c40b287c948e
limber <limbermay21@gmail.com>
2022-02-05 15:11:15 -0600

    se hicieron configuraciones de estilo y posicionamiento al objeto botón

--- ventana_config/opcion.py
+++ ventana_config/opcion.py
@@ -6,11 +6,24 @@ class Opcion(Frame):
 
         # Atributos de la clase
         self.texto = texto
-        self.lista_combobox = lista_combobox
 
         # Configuraciones del frame
         self.config(
-            bg="white",
+            bg="#2c2b33",
             width=350,
             height=30
         )
+
+        # Configuraciones de las partes del frame
+        self.texto = Label(self, text=self.texto)
+
+        # Configuracion de estilos de las partes del frame
+        self.texto.config(
+            fg="white",
+            font=("ocean", 11),
+            bg="#2c2b33"
+        )
+
+        # Posicionamiento de las partes del frame
+        self.texto.grid(row=0, column=0)
+        self.lista_combobox.grid(row=0, column=1)
