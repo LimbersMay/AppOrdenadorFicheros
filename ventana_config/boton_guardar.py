19ee106de3141bd34375a5ff7452f59d428072e1
limber <limbermay21@gmail.com>
2022-02-05 16:17:09 -0600

    se agregó la configuración básica del botón

--- ventana_config/boton_guardar.py
+++ ventana_config/boton_guardar.py
@@ -3,3 +3,10 @@ from tkinter import *
 class BotonGuardar(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
+
+        # Configuraciones del frame
+        self.config(
+            bg="#2c2b33",
+            width=370,
+            height=50
+        )
