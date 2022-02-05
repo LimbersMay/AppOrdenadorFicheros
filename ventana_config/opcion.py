bff94bb8f249c2ac83c6f9e9825f5f5c30848f35
limber <limbermay21@gmail.com>
2022-02-05 15:47:14 -0600

    se agregaron configuraciones de columnas y se posicionaron los elementos de opcion

--- ventana_config/opcion.py
+++ ventana_config/opcion.py
@@ -1,17 +1,19 @@
 from tkinter import *
+from tkinter.ttk import Combobox
 
 class Opcion(Frame):
-    def __init__(self, master, texto = "", lista_combobox = None):
+    def __init__(self, master, texto = ""):
         Frame.__init__(self, master)
 
         # Atributos de la clase
         self.texto = texto
+        self.lista_combobox = Combobox(self)
 
         # Configuraciones del frame
         self.config(
             bg="#2c2b33",
-            width=350,
-            height=30
+            width=280,
+            height=20
         )
 
         # Configuraciones de las partes del frame
@@ -24,9 +26,14 @@ class Opcion(Frame):
             bg="#2c2b33"
         )
 
+        self.grid_propagate(False)
+
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
         # Posicionamiento de las partes del frame
-        self.texto.grid(row=0, column=0)
-        self.lista_combobox.grid(row=0, column=1)
+        self.texto.grid(row=0, column=0, sticky=W)
+        self.lista_combobox.grid(row=0, column=1, sticky=E)
     
     # Método para enviarle al objeto Combobox la lista de opciones
     def enviar_lista_opciones(self, lista_opciones):
